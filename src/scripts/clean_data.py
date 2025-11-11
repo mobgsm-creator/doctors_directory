9b69845db81a0a29767ee008f16f73ed7d8e0f05
Agney <agneynalapat123@gmail.com>
2025-11-11 20:32:17 +0530

    supabase endpoint

---
+++ src/scripts/clean_data.py
@@ -0,0 +1,237 @@
+import pandas as pd
+import json
+import textwrap
+import re
+
+
+
+patterns = {
+    "x_twitter": r"(https?://(?:www\.)?(?:x\.com|twitter\.com)/[A-Za-z0-9_/?=&\.-]+)",
+    "facebook": r"(https?://(?:www\.)?facebook\.com/[A-Za-z0-9_/?=&\.-]+)",
+    "instagram": r"(https?://(?:www\.)?instagram\.com/[A-Za-z0-9_/?=&\.-]+)",
+    "youtube": r"(https?://(?:www\.)?(?:youtube\.com|youtu\.be)/[A-Za-z0-9_/?=&\.-]+)",
+    "linkedin": r"(https?://(?:www\.)?linkedin\.com/[A-Za-z0-9_/?=&\.-]+)",
+    "website": r"(https?://[A-Za-z0-9_\-\.]+\.[A-Za-z]{2,}(?:/[^\s]*)?)",
+    "email": r"([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,})",
+    "cqc": r"(https?://(?:www\.)?(?:cqc\.org\.uk)/location/[A-Za-z0-9_/?=&\.-]+)",
+    "hiw": r"(https?://(?:www\.)?(?:hiw\.org\.uk)/[A-Za-z0-9_/?=&\.-]+)",
+    "his": r"(https?://(?:www\.)?(?:healthcareimprovementscotland\.scot)/[A-Za-z0-9_/?=&\.-]+)",
+    "rqia": r"(https?://(?:www\.)?(?:rqia\.org\.uk)(?:/[^\s]*)?)"
+}
+df= pd.read_excel(r"C:\Users\agney\Downloads\5600_UK_slug_search_results_2k_.xlsx",sheet_name='exists')
+print(len(df))
+def parse_accreditations(accreditations):
+    result = ""
+    try:
+        if type(accreditations) == list:
+            try:
+                result = "".join(accreditations)
+            except Exception as e:
+                result = "".join(accreditations[0])
+        elif type(accreditations) == dict:
+            result="".join([key+": "+accreditations[key]+"\n" for key in accreditations.keys()])
+        elif type(accreditations) == str:
+            result="\nAccreditations: "+accreditations
+    except Exception as e:
+        pass
+    return result
+
+
+for index,row in df.iterrows():
+    try:
+        data_dict=json.loads(re.sub(r':contentReference\[oaicite:\d+\]\{index=\d+\}', '', json.loads(row['json_response'])))
+        try:
+            about_section=data_dict['ABOUT']+parse_accreditations(data_dict['ACCREDITATIONS'])
+        except Exception as e:
+            pass
+        try:
+            about_section=about_section+parse_accreditations(data_dict['AWARDS'])
+        except Exception as e:
+            pass
+        try:
+            about_section=about_section+parse_accreditations(data_dict['AFFILIATIONS'])
+        except Exception as e:
+            pass
+        # try:
+        #     compliance_section=data_dict['REGULATORY_BODY']+"".join([key+": "+data_dict['CQC_OR_EQUIVALENT_REPORT'][key]+"\n" for key in data_dict['CQC_OR_EQUIVALENT_REPORT'].keys()])
+        # except Exception as e:
+        #     print("Compliance section Exception",e)
+        #     print(data_dict['CQC_OR_EQUIVALENT_REPORT'])
+        #     pass
+        hours = data_dict['HOURS']
+        try:
+            found = re.findall(patterns['facebook'], data_dict['SOCIAL_MEDIA']['Facebook'])
+            df.at[index,'facebook']=found[0] if found else None
+        except Exception as e:
+            df.at[index,'facebook']=None
+
+            pass
+        try:
+            found = re.findall(patterns['x_twitter'], data_dict['SOCIAL_MEDIA']['Twitter'])
+            df.at[index,'x_twitter']=found[0] if found else None
+
+        except Exception as e:
+            df.at[index,'twitter']=None
+            pass
+        try:
+            found = re.findall(patterns['linkedin'], data_dict['SOCIAL_MEDIA']['LinkedIn'])
+            df.at[index,'Linkedin']=found[0] if found else None
+        except Exception as e:
+            for item in data_dict["PRACTITIONERS"]:
+                try:
+                    if item['LinkedinURL'].startswith('https://'):
+                        found = re.findall(patterns['linkedin'], data_dict['SOCIAL_MEDIA']['LinkedIn'])
+                        df.at[index,'Linkedin']=found[0] if found else None
+                        break
+                except Exception as e:
+                    df.at[index,'Linkedin']=None
+                    pass
+            pass
+        try:
+            found = re.findall(patterns['instagram'], data_dict['SOCIAL_MEDIA']['Instagram'])
+            df.at[index,'instagram']=found[0] if found else None
+        except Exception as e:
+            df.at[index,'instagram']=None
+            pass
+
+        
+        try:
+            found = re.findall(patterns['instagram'], data_dict['SOCIAL_MEDIA']['YouTube'])
+            df.at[index,'instagram']=found[0] if found else None
+        except Exception as e:
+            df.at[index,'youtube']=None
+            pass
+        try:
+            found = re.findall(patterns['website'], data_dict['SOCIAL_MEDIA']['Website'])
+            df.at[index,'website']=found[0] if found else None
+        except Exception as e: 
+            try:
+                found = re.findall(patterns['website'], data_dict['SOURCE_LINKS'][0])
+                df.at[index,'website']=found[0] if found else None
+            except Exception as e:
+                df.at[index,'website']=None
+                pass
+
+            pass
+            
+        try:
+            found = re.findall(patterns['email'], data_dict['CONTACT']['Email'])
+            df.at[index,'email']=found[0] if found else None
+        except Exception as e:
+            df.at[index,'email']=None
+            pass
+        
+        try:
+            save_face = json.dumps(data_dict['ACCREDITATIONS']).lower()
+            isSaveFace=False
+            if "save face" in save_face or "saveface" in save_face:
+                isSaveFace=True
+            df.at[index,'isSaveFace']=isSaveFace
+        except Exception as e:
+            pass
+        
+        try:
+            qualification_keywords = [
+                "MBBS", "MBCHB", "MRCGP", "PGDIP", "GMC", "NMC", 
+                "DOCTOR", "DERMATOLOGY", "MEDICAL", "CLINICAL", "REGISTERED NURSE"]
+            # Convert practitioner data to lowercase string
+            practitioners_text = json.dumps(data_dict['PRACTITIONERS']).lower()
+            
+            # Check if any keyword appears in the text
+            isDoctor = any(keyword.lower() in practitioners_text for keyword in qualification_keywords)
+            df.at[index,'isDoctor']=isDoctor
+        except Exception as e:
+            pass
+        
+        try:
+            #https://www.rqia.org.uk/RQIA/media/CareServices
+            isCQC=[False,""]
+            isHIW=[False,""]
+            isHIS=[False,""]
+            isJCCP=[False,""]
+            isRQIA=[False,""]
+            compliance_test = json.dumps(data_dict).lower() 
+
+            if "https://www.cqc.org.uk/location" in compliance_test:
+                found = re.findall(patterns['cqc'], compliance_test)
+                isCQC=[True, found[0] if found else None]
+            try:
+                if "https://www.hiw.org.uk/" in compliance_test:
+                    found = re.findall(patterns['hiw'], compliance_test)
+                    value = found[0].replace("//","/").replace(".","") if found else None
+                    if len(value.split("/"))==2:
+                        isHIW=[True, value]
+
+                    isHIW=[True, found[0] if found else None]
+            except Exception as e:
+                pass
+            if "https://www.healthcareimprovementscotland.scot/" in compliance_test:
+                found = re.findall(patterns['his'], compliance_test)
+                value = found[0] if found else None
+                if "services" in value:
+                    isHIS=[True, value]
+            if "jccp" in compliance_test:
+                isJCCP=[True, ""]
+
+            if "https://www.rqia.org.uk" in compliance_test:
+                found = re.findall(patterns['rqia'], compliance_test)
+                isRQIA=[True, found[0] if found else None]
+            df.at[index, 'isJCCP']=str(isJCCP)
+            df.at[index,'isCQC']=str(isCQC)
+            df.at[index,'isHIW']=str(isHIW)
+            df.at[index,'isHIS']=str(isHIS)
+            df.at[index,'isRQIA']=str(isRQIA)
+        except Exception as e:
+            print("ere",e)
+            pass
+
+        try:
+
+            df.at[index,'about_section']=about_section
+            #df.at[index,'compliance_section']=compliance_section
+            df.at[index,'hours']=json.dumps(hours)
+            df.at[index,'Practitioners']=json.dumps(data_dict['PRACTITIONERS'])
+            df.at[index, 'Insurace'] = json.dumps(data_dict['INSURANCE_ACCEPTED'])
+            df.at[index, 'Payments'] = json.dumps(data_dict['PAYMENT_OPTIONS'])
+            df.at[index, 'Fees'] = json.dumps(data_dict['ESTIMATED_FEES'])
+        except Exception as e:
+            pass
+        
+        
+                
+                
+                        #for key in data_dict.keys():
+            #print(key,data_dict[key],"\n\n----------------------------\n\n")
+            
+            #Check if CQC is present in ACCREDITATIONS and /location link available in either source_links or  CQC_OR_EQUIVALENT_REPORT
+            #Check if practitioners have propper qualifications
+
+                      # if key=='EDUCATION':
+            #     df.at[index,key]=data_dict[key][0]['Institution']
+            # else:
+
+            #     try:
+            #         df.at[index,key]=data_dict[key].split(":contentReference")[0]
+            #     except Exception as e:
+            #         df.at[index,key]=json.dumps(data_dict[key]).split(":contentReference")[0]
+    except json.JSONDecodeError as e:
+        # Print and write debug info to a file
+        error_info = (
+            f"{index}❌ JSON Decode Error:\n"
+            f"Message: {e.msg}\n"
+            f"Line: {e.lineno}, Column: {e.colno}, Char index: {e.pos}\n\n"
+        )
+        print(error_info)
+ 
+        
+
+        # # Write the error info to file
+        with open(f"json_error_debug_{index}.txt", "w", encoding="utf-8") as f:
+            f.write(e.doc)
+        
+        # print("JSON error details written to json_error_debug.txt")
+        # print(f"\n{error_info}")
+        pass
+
+
+df.to_csv("test1.csv")
--- src/scripts/create_json.py
+++ src/scripts/create_json.py
@@ -2,15 +2,20 @@
 import pandas as pd
 from urllib.parse import unquote_plus
 import json
+import time
+import numpy as np
+import re
 JCCP_Greens = r"C:\Users\agney\Documents\Files\Projects\UK-Dermatologists\prod_DB\JCCP_Greens_P.csv"
 JCCP_Yellows = r"C:\Users\agney\Documents\Files\Projects\UK-Dermatologists\prod_DB\JCCP_Yellow_C.csv"
 doctify_jccp = r"C:\Users\agney\Documents\Files\Projects\UK-Dermatologists\prod_DB\practitioners\DB.xlsx"
 # Path to your CSV file
 csv_file = doctify_jccp 
-
-# Load CSV
-#df = pd.read_csv(csv_file)
-df = pd.read_excel(r"C:\Users\agney\Documents\Files\Projects\UK-Dermatologists\prod_DB\clinics\5600_UK_slug.xlsx",sheet_name='Sheet2')
+def slugify(value: str) -> str:
+    value = value.lower().strip()
+    value = re.sub(r'[^a-z0-9]+', '-', value)  # replace non-alphanumeric with '-'
+    value = re.sub(r'-+', '-', value)          # collapse multiple '-'
+    value = re.sub(r'^-|-$', '', value)        # remove leading/trailing '-'
+    return value
 
 # def clean(x):
 #     if "Show less" in x:
@@ -20,13 +25,16 @@ df = pd.read_excel(r"C:\Users\agney\Documents\Files\Projects\UK-Dermatologists\p
 #     else: 
 #         return x
 # df['SPECIALTIES'] = df['SPECIALTIES'].apply(lambda x: clean(x))
-
+df = pd.read_csv(r"C:\Users\agney\Documents\Files\Projects\doctor-directory\src\scripts\test-1.csv")
 # Convert DataFrame to list of dicts
-devices_list = df[:500].to_dict(orient='records')
+df['slug'] = df['slug'].apply(slugify)
+df = df.replace([None, np.nan, "None"], "")
+df.to_csv("Clinics3.5k.csv")
+devices_list = df.to_dict(orient='records')
 
 
 
-# # Write JSON file
+# # # Write JSON file
 with open('clinics.json', 'w', encoding='utf-8') as f:
     json.dump(devices_list, f, indent=2, ensure_ascii=False)
 
