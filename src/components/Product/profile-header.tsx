9fa7a16260110ee3a2b1570a0d6f575ab39937fc
abdul <a.rehman553@gmail.com>
2025-12-15 15:52:08 +0500

    product page styling

--- src/components/Product/profile-header.tsx
+++ src/components/Product/profile-header.tsx
@@ -10,8 +10,7 @@ import {
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import type { Product} from "@/lib/types";
-
+import type { Product } from "@/lib/types";
 
 import { cn } from "@/lib/utils";
 
@@ -29,29 +28,26 @@ export function ProfileHeader({ clinic }: ProfileHeaderProps) {
   return (
     <Card className="md:mt-2 flex flex-col gap-6 md:rounded-xl px-0 md:px-6 py-6 relative shadow-none group transition-all duration-300 md:rounded-27 border-t border-b border-[#C4C4C4] md:border-t-[1px] md:border md:border-[var(--alto)] bg-white md:bg-[var(--primary-bg-color)]">
       <div className="px-4 md:px-0 grid grid-cols-1 lg:grid-cols-[4fr_1fr] gap-4 items-center">
-        <div className="flex flex-row flex-wrap pb-4 md:pb-0 md:mb-4 md:px-4 md:px-0 lg:mb-0 items-center border-b border-[#C4C4C4] md:border-0">
+        <div className="flex flex-row flex-wrap pb-4 md:pb-0 md:mb-4 px-0 lg:mb-0 items-start border-b border-[#C4C4C4] md:border-0">
           {/* Profile Image Section */}
-          <div className="flex flex-col items-center w-[115px] md:w-[140px] mr-2 md:mr-4">
+          <div className="flex flex-col items-center w-[115px] md:w-[200px] mr-2 md:mr-4">
             <div className="relative group">
               {/* Elegant frame with shadow */}
               <div className="absolute -inset-1 rounded-full blur opacity-25 transition duration-300" />
-                <div className="w-[80px] h-[80px] md:w-[140px] md:h-[140px] flex items-center justify-center overflow-hidden rounded-full bg-grey-300">
-                  <img
-                    src={
-                      clinic.image_url?.replaceAll('"', "") ||
-                      "/placeholder.svg"
-                    }
-                    alt={practitionerName}
-                    className="object-cover rounded-full min-w-full min-h-full"
-                  />
-                </div>
-
+              <div className="w-[80px] h-[80px] md:w-[200px] md:h-[200px] flex items-center justify-center overflow-hidden rounded-lg bg-grey-300">
+                <img
+                  src={
+                    clinic.image_url?.replaceAll('"', "") || "/placeholder.svg"
+                  }
+                  alt={practitionerName}
+                  className="object-cover rounded-lg min-w-full min-h-full"
+                />
+              </div>
             </div>
-
           </div>
 
           {/* Main Information Section */}
-          <div className="gap-0 flex items-start flex-col w-[calc(100%-123px)] md:w-[calc(100%-180px)]">
+          <div className="gap-0 flex items-start flex-col w-[calc(100%-123px)] md:w-[calc(100%-230px)]">
             {/* Name and credentials */}
             <div className="flex w-full md:w-auto flex-col md:flex-row gap-2 mb-2 items-start md:items-center">
               <h1 className="inline text-left mb-0 md:m-0 font-semibold text-md md:text-2xl transition-colors md:text-left">
@@ -59,66 +55,48 @@ export function ProfileHeader({ clinic }: ProfileHeaderProps) {
               </h1>
             </div>
 
-            <div className="flex flex-row gap-2 mb-3 items-center">
-              <p className="text-pretty md:font-bold text-sm md:text-md">{roleTitle}</p>
-              
+            <div className="flex flex-row gap-2 mb-0 items-center">
+              <p className="text-pretty md:font-bold text-sm md:text-md">
+                {roleTitle}
+              </p>
+              <span className="relative pl-3 text-sm before:absolute before:left-0 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full before:bg-black">
+                {clinic.product_category}
+              </span>
             </div>
 
             <div className="hidden md:block">
-                {/* Contact Information */}
+              {/* Contact Information */}
 
-                <div className="flex items-start gap-3 my-2">
-                  <MapPin className="h-4 w-4 mt-1 shrink-0 " />
-                  <span className="text-sm">{clinic.product_category}</span>
-                </div>
-
-                <div className="flex flex-wrap gap-4 my-2">
-                
-                    <Phone className="h-3.5 w-3.5 mr-1.5" />
-                    <span>{clinic.product_subcategory}</span>
-              
-
-                </div>
-
-                {/* Specializations preview */}
-                <div className="flex flex-wrap gap-2 pt-2">
-                  {clinic.certifications_and_compliance?.map(
-                    (spec:string) => (
-                      <Badge
-                        key={spec}
-                        variant="outline"
-                        className="border-black  hover:text-black transition-colors bg-[var(--primary-bg-color)]"
-                      >
-                        {spec
-                          .split("-")
-                          .map(
-                            (word) => word.charAt(0).toUpperCase() + word.slice(1)
-                          )
-                          .join(" ")}
-                      </Badge>
-                    )
-                  )}
-                </div>
+              <div className="flex flex-wrap gap-4 my-2">
+                <span>{clinic.product_subcategory}</span>
+              </div>
             </div>
 
+            {/* Specializations preview */}
+            <div className="flex flex-wrap gap-2 pt-2">
+              {clinic.certifications_and_compliance?.map((spec: string) => (
+                <Badge
+                  key={spec}
+                  variant="outline"
+                  className="flex items-center gap-1 rounded-full bg-green-100 text-green-800 border border-green-300 text-xs px-3 py-1 mb-1"
+                >
+                  {spec
+                    .split("-")
+                    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                    .join(" ")}
+                </Badge>
+              ))}
+            </div>
           </div>
         </div>
 
         <div className="block md:hidden">
-                {/* Contact Information */}
+          {/* Contact Information */}
 
-                <div className="flex items-start gap-3 my-2">
-                  <MapPin className="h-4 w-4 mt-1 shrink-0 " />
-                  <span className="text-sm">{clinic.brand_about}</span>
-                  <span className='text-sm'>{clinic.seller_about}</span>
-                </div>
-
-            
-
-   
-            </div>
-
-       
+          <div className="flex flex-wrap gap-4 my-2">
+            <span>{clinic.product_subcategory}</span>
+          </div>
+        </div>
       </div>
     </Card>
   );
--- src/components/practitioner-card.tsx
+++ src/components/practitioner-card.tsx
@@ -22,7 +22,6 @@ function isProduct(obj: unknown): obj is Product {
   return typeof obj === "object" && obj !== null && "product_name" in obj;
 }
 
-
 export function PractitionerCard({ practitioner }: PractitionerCardProps) {
   const practitionerName = (practitioner as Practitioner).practitioner_name
     ? (practitioner as Practitioner).practitioner_name
@@ -33,211 +32,200 @@ export function PractitionerCard({ practitioner }: PractitionerCardProps) {
         .split("-")
         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
         .join(" ");
-  try{
-    console.log((practitioner as Product).image_url)
-  }
-  catch{
-    console.log("error")
+  try {
+    console.log((practitioner as Product).image_url);
+  } catch {
+    console.log("error");
   }
 
   return (
     <>
-  {(isPractitioner(practitioner) || isClinic(practitioner)) && (
-    <Card className="gap-0 relative px-4 md:px-0 shadow-none group transition-all duration-300 border-b border-t-0 border-[#C4C4C4] md:border-t-[1px] rounded-27 md:border md:border-[var(--alto)] cursor-pointer">
-      
-      <CardHeader className="pb-4 px-2">
-        <div className="flex items-start gap-4">
-          <div className="text-center flex-1 min-w-0 items-center flex flex-col">
-            <div className="flex w-full flex-row items-start border-b border-[#C4C4C4] md:border-0 md:flex-col md:items-center">
-              <div className="w-[80px] h-[80px] md:w-[150px] md:h-[150px] flex items-center justify-center overflow-hidden rounded-full bg-gray-300 md:mb-3 mr-0">
-                <img
-                  src={
-                    practitioner.image.replace("&w=256&q=75", "") ||
-                    "/placeholder.svg"
-                  }
-                  alt="Profile photo"
-                  className="object-cover rounded-full min-w-full min-h-full"
-                  onError={(e) => {
-                    e.currentTarget.onerror = null; // prevent infinite loop
-                    e.currentTarget.src = "/directory/images/doc.png";
-                  }}
-                />
-              </div>
-
-              <div className="flex items-start md:items-center flex-col pl-4 md:pl-0 w-[calc(100%-80px)] md:w-full">
-                <h3 className="mb-2 md:mb-4 flex text-left md:text-center md:align-items-center md:justify-center font-semibold text-md md:text-lg transition-colors text-balance">
-                  {practitionerName}
-                </h3>
-
-                {!("profession" in practitioner) && (
-                  <>
-                    <ClinicLabels clinic={practitioner} />
-                  </>
-                )}
-
-                {"practitioner_image_link" in practitioner &&
-                  (practitioner as Practitioner).practitioner_name && (
-                    <p className="text-muted-foreground mb-2 text-pretty">
-                      {(practitioner as Practitioner).practitioner_title.trim()}
-                    </p>
-                  )}
-
-                {!("practitioner_image_link" in practitioner) &&
-                  practitioner.category && (
-                    <p className="pt-2 mb-2">
-                      {practitioner.category.trim()}
-                    </p>
-                  )}
-              </div>
-            </div>
-
-            <div className="flex flex-row gap-2 pt-3 items-center justify-start md:justify-center w-full text-sm">
-              <div className="inline-flex items-center gap-1">
-                <div className="flex items-center">
-                  {Array.from({ length: 5 }, (_, i) => (
-                    <Star
-                      key={i}
-                      className={`h-4 w-4 ${
-                        i < practitioner.rating
-                          ? "fill-black text-black"
-                          : "text-muted-foreground/30"
-                      }`}
+      {(isPractitioner(practitioner) || isClinic(practitioner)) && (
+        <Card className="gap-0 relative px-4 md:px-0 shadow-none group transition-all duration-300 border-b border-t-0 border-[#C4C4C4] md:border-t-[1px] rounded-27 md:border md:border-[var(--alto)] cursor-pointer">
+          <CardHeader className="pb-4 px-2">
+            <div className="flex items-start gap-4">
+              <div className="text-center flex-1 min-w-0 items-center flex flex-col">
+                <div className="flex w-full flex-row items-start border-b border-[#C4C4C4] md:border-0 md:flex-col md:items-center">
+                  <div className="w-[80px] h-[80px] md:w-[150px] md:h-[150px] flex items-center justify-center overflow-hidden rounded-full bg-gray-300 md:mb-3 mr-0">
+                    <img
+                      src={
+                        practitioner.image.replace("&w=256&q=75", "") ||
+                        "/placeholder.svg"
+                      }
+                      alt="Profile photo"
+                      className="object-cover rounded-full min-w-full min-h-full"
+                      onError={(e) => {
+                        e.currentTarget.onerror = null; // prevent infinite loop
+                        e.currentTarget.src = "/directory/images/doc.png";
+                      }}
                     />
-                  ))}
+                  </div>
+
+                  <div className="flex items-start md:items-center flex-col pl-4 md:pl-0 w-[calc(100%-80px)] md:w-full">
+                    <h3 className="mb-2 md:mb-4 flex text-left md:text-center md:align-items-center md:justify-center font-semibold text-md md:text-lg transition-colors text-balance">
+                      {practitionerName}
+                    </h3>
+
+                    {!("profession" in practitioner) && (
+                      <>
+                        <ClinicLabels clinic={practitioner} />
+                      </>
+                    )}
+
+                    {"practitioner_image_link" in practitioner &&
+                      (practitioner as Practitioner).practitioner_name && (
+                        <p className="text-muted-foreground mb-2 text-pretty">
+                          {(
+                            practitioner as Practitioner
+                          ).practitioner_title.trim()}
+                        </p>
+                      )}
+
+                    {!("practitioner_image_link" in practitioner) &&
+                      practitioner.category && (
+                        <p className="pt-2 mb-2">
+                          {practitioner.category.trim()}
+                        </p>
+                      )}
+                  </div>
+                </div>
+
+                <div className="flex flex-row gap-2 pt-3 items-center justify-start md:justify-center w-full text-sm">
+                  <div className="inline-flex items-center gap-1">
+                    <div className="flex items-center">
+                      {Array.from({ length: 5 }, (_, i) => (
+                        <Star
+                          key={i}
+                          className={`h-4 w-4 ${
+                            i < practitioner.rating
+                              ? "fill-black text-black"
+                              : "text-muted-foreground/30"
+                          }`}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                  <span className="border-l border-black pl-2 underline">
+                    ({practitioner.reviewCount} reviews)
+                  </span>
                 </div>
               </div>
-              <span className="border-l border-black pl-2 underline">
-                ({practitioner.reviewCount} reviews)
+            </div>
+          </CardHeader>
+
+          <CardContent className="pt-0 px-0 md:px-4 space-y-4">
+            <div className="flex items-start gap-2 text-sm">
+              <MapPin className="h-4 w-4 mt-0 flex-shrink-0" />
+              <span className="text-pretty">
+                {practitioner.gmapsAddress.split(",")[
+                  practitioner.gmapsAddress.split(",").length - 2
+                ] +
+                  ", " +
+                  practitioner.gmapsAddress.split(",")[
+                    practitioner.gmapsAddress.split(",").length - 1
+                  ]}
               </span>
             </div>
-          </div>
-        </div>
-      </CardHeader>
 
-      <CardContent className="pt-0 px-0 md:px-4 space-y-4">
-        <div className="flex items-start gap-2 text-sm">
-          <MapPin className="h-4 w-4 mt-0 flex-shrink-0" />
-          <span className="text-pretty">{practitioner.gmapsAddress.split(",")[practitioner.gmapsAddress.split(",").length-2]+", "+practitioner.gmapsAddress.split(",")[practitioner.gmapsAddress.split(",").length-1]}</span>
-        </div>
+            <Link
+              href={
+                "practitioner_image_link" in practitioner &&
+                (practitioner as Practitioner).practitioner_name
+                  ? `/profile/${
+                      (practitioner as Practitioner).practitioner_name
+                    }`
+                  : `/clinics/${(practitioner as Clinic).City}/clinic/${
+                      practitioner.slug
+                    }`
+              }
+              className="mt-4 mb-0 flex border rounded-lg font-weight px-4 py-2 bg-black align-items-center justify-center text-white hover:bg-white hover:text-black"
+            >
+              Contact
+            </Link>
 
-        <Link
-          href={
-            "practitioner_image_link" in practitioner &&
-            (practitioner as Practitioner).practitioner_name
-              ? `/profile/${(practitioner as Practitioner).practitioner_name}`
-              : `/clinics/${(practitioner as Clinic).City}/clinic/${
-                  practitioner.slug
-                }`
-          }
-          className="mt-4 mb-0 flex border rounded-lg font-weight px-4 py-2 bg-black align-items-center justify-center text-white hover:bg-white hover:text-black"
-        >
-          Contact
-        </Link>
+            {practitioner.reviewAnalysis?.procedures_offered?.categories
+              .length! > 0 && null}
 
-        {practitioner.reviewAnalysis?.procedures_offered?.categories.length! >
-          0 && null}
-
-        <div>
-          <div className="flex flex-wrap gap-1 pt-4">
-            {true &&
-              practitioner.reviewAnalysis?.procedures_offered?.categories
-                .slice(0, 2)
-                .map((modality, index) => (
-                  <Badge key={index} variant="outline" className="text-xs">
-                    {modality
-                      .split(" ") // split into words
-                      .map(
-                        (word) => word.charAt(0).toUpperCase() + word.slice(1)
-                      ) // capitalize each
-                      .join(" ")}
-                  </Badge>
-                ))}
-            {true &&
-              practitioner.reviewAnalysis?.procedures_offered?.categories
-                ?.length! > 2 && (
-                <Badge variant="outline" className="text-xs">
-                  +
-                  {practitioner.reviewAnalysis?.procedures_offered?.categories!
-                    .length! - 2}{" "}
-                  more
-                </Badge>
-              )}
-          </div>
-        </div>
-      </CardContent>
-    </Card>
-  )}
-  {isProduct(practitioner) && (
-    <Card className="gap-0 relative px-4 md:px-0 shadow-none group transition-all duration-300 border-b border-t-0 border-[#C4C4C4] md:border-t-[1px] rounded-27 md:border md:border-[var(--alto)] cursor-pointer">
-      
-      <CardHeader className="pb-4 px-2">
-        <div className="flex items-start gap-4">
-          <div className="text-center flex-1 min-w-0 items-center flex flex-col">
-            <div className="flex w-full flex-row items-start border-b border-[#C4C4C4] md:border-0 md:flex-col md:items-center">
-              <div className="w-[80px] h-[80px] md:w-[150px] md:h-[150px] flex items-center justify-center overflow-hidden rounded-full bg-gray-300 md:mb-3 mr-0">
-                <img
-                  src={
-                    practitioner.image_url?.replaceAll('"', "") ||
-                    "/placeholder.svg"
-                  }
-                  alt="Profile photo"
-                  className="object-cover rounded-full min-w-full min-h-full"
-                  
-                />
-              </div>
-
-              <div className="flex items-start md:items-center flex-col pl-4 md:pl-0 w-[calc(100%-80px)] md:w-full">
-                <h3 className="mb-2 md:mb-4 flex text-left md:text-center md:align-items-center md:justify-center font-semibold text-md md:text-lg transition-colors text-balance">
-                  {practitioner.product_name}
-                </h3>
-
-                
-
-                {
-                  (practitioner.product_name && (
-                    <p className="text-muted-foreground mb-2 text-pretty">
-                      {practitioner?.brand?.trim()}
-                    </p>
-                  ))}
-
+            <div>
+              <div className="flex flex-wrap gap-1 pt-4">
+                {true &&
+                  practitioner.reviewAnalysis?.procedures_offered?.categories
+                    .slice(0, 2)
+                    .map((modality, index) => (
+                      <Badge key={index} variant="outline" className="text-xs">
+                        {modality
+                          .split(" ") // split into words
+                          .map(
+                            (word) =>
+                              word.charAt(0).toUpperCase() + word.slice(1)
+                          ) // capitalize each
+                          .join(" ")}
+                      </Badge>
+                    ))}
+                {true &&
+                  practitioner.reviewAnalysis?.procedures_offered?.categories
+                    ?.length! > 2 && (
+                    <Badge variant="outline" className="text-xs">
+                      +
+                      {practitioner.reviewAnalysis?.procedures_offered
+                        ?.categories!.length! - 2}{" "}
+                      more
+                    </Badge>
+                  )}
               </div>
             </div>
+          </CardContent>
+        </Card>
+      )}
+      {isProduct(practitioner) && (
+        <Link href={`/products/${practitioner.slug}`} className="block">
+          <Card className="gap-0 h-full relative px-4 md:px-0 shadow-none group transition-all duration-300 border-b border-t-0 border-[#C4C4C4] md:border-t-[1px] rounded-27 md:border md:border-[var(--alto)] cursor-pointer">
+            <CardHeader className="pb-2 px-2">
+              <div className="flex items-start gap-4">
+                <div className="text-center flex-1 min-w-0 items-center flex flex-col">
+                  <div className="flex w-full flex-row items-start border-b border-[#C4C4C4] md:border-0 md:flex-col md:items-center">
+                    <div className="w-[80px] h-[80px] md:w-[150px] md:h-[150px] flex items-center justify-center overflow-hidden rounded-lg bg-gray-300 md:mb-3 mr-0">
+                      <img
+                        src={
+                          practitioner.image_url?.replaceAll('"', "") ||
+                          "/placeholder.svg"
+                        }
+                        alt="Profile photo"
+                        className="object-cover rounded-lg min-w-full min-h-full"
+                      />
+                    </div>
 
-       
-          </div>
-        </div>
-      </CardHeader>
+                    <div className="flex items-start md:items-center flex-col pl-4 md:pl-0 w-[calc(100%-80px)] md:w-full">
+                      {practitioner.product_name && (
+                        <p className="flex items-center gap-1 rounded-full bg-green-100 text-green-800 border border-green-300 text-xs px-3 py-1 mb-4">
+                          {practitioner?.brand?.trim()}
+                        </p>
+                      )}
 
-      <CardContent className="pt-0 px-0 md:px-4 space-y-4">
-        <div className="flex items-start gap-2 text-sm">
-          <MapPin className="h-4 w-4 mt-0 flex-shrink-0" />
-          <span className="text-pretty">{practitioner.manufacturer?.trim()}</span>
-        </div>
+                      <h3 className="mb-3 md:mb-0 flex text-left md:text-center md:align-items-center md:justify-center font-semibold text-md md:text-lg transition-colors text-balance">
+                        {practitioner.product_name}
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </CardHeader>
 
-        <Link
-          href={
-           
-          
-             `/products/${practitioner.slug}`
-    
-                }
-          
-          className="mt-4 mb-0 flex border rounded-lg font-weight px-4 py-2 bg-black align-items-center justify-center text-white hover:bg-white hover:text-black"
-        >
-          Contact
-        </Link>
-
-        <div>
-          <div className="flex flex-wrap gap-1 pt-4">
-            {true &&
-              practitioner.all_prices
-                .map((value: any, index:number) => (
-                  <Badge key={index} variant="outline" className="text-xs">
-                    {value.price
-                      }
-                  </Badge>
-                ))}
-            {/* {true &&
+            <CardContent className="pt-0 px-0 md:px-4 space-y-4">
+              <div className="flex md:items-center md:justify-center gap-2 text-sm">
+                <span className="text-pretty text-center">
+                  {practitioner.manufacturer?.trim()}
+                </span>
+              </div>
+              <div>
+                <div className="flex flex-wrap md:items-center md:justify-center gap-1 text-center">
+                  {true &&
+                    practitioner.all_prices.map((value: any, index: number) => (
+                      <Badge key={index} variant="outline" className="text-xs">
+                        {value.price}
+                      </Badge>
+                    ))}
+                  {/* {true &&
               practitioner.reviewAnalysis?.procedures_offered?.categories
                 ?.length! > 2 && (
                 <Badge variant="outline" className="text-xs">
@@ -247,12 +235,12 @@ export function PractitionerCard({ practitioner }: PractitionerCardProps) {
                   more
                 </Badge>
               )} */}
-          </div>
-        </div>
-      </CardContent>
-    </Card>
-  )}
-
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
+      )}
     </>
   );
 }
