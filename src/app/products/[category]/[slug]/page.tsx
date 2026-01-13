eed6fbad6075fc6024da5dd79a4fa39293fb8a87
Agney <agneynalapat123@gmail.com>
2026-01-13 20:32:27 +0530

    product browse more

--- src/app/products/[category]/[slug]/page.tsx
+++ src/app/products/[category]/[slug]/page.tsx
@@ -121,6 +121,83 @@ export default async function ProfilePage({ params }: ProfilePageProps) {
          
         
         </div>
+        <h2 className="text-lg font-semibold text-foreground mb-2">{`Browse more ${clinic.product_category}`}</h2>
+           <div className="flex overflow-x-auto gap-4 pb-4 snap-x snap-mandatory animate-fade-in no-scrollbar">
+          {similarProducts.map((practitioner, index) => 
+            (
+            <div key={practitioner.product_name}>
+
+                <Link href={`/products/${practitioner.category}/${practitioner.slug}`} className="block">
+          <Card className="gap-0 h-full w-100 relative px-4 md:px-0 shadow-none group transition-all duration-300 border-b border-t-0 border-[#C4C4C4] md:border-t-[1px] rounded-27 md:border md:border-[var(--alto)] cursor-pointer" aria-labelledby={`product-name-${practitioner.slug}`}>
+            <CardHeader className="pb-2 px-2">
+              <h2 id={`product-name-${practitioner.slug}`} className="sr-only">
+                {decodeUnicodeEscapes(practitioner.product_name)}
+              </h2>
+              <div className="flex items-start gap-4">
+                <div className="text-center flex-1 min-w-0 items-center flex flex-col">
+                  <div className="flex w-full flex-row items-start border-b border-[#C4C4C4] md:border-0 md:flex-col md:items-center">
+                    <div className="w-[80px] h-[80px] md:w-[150px] md:h-[150px] flex items-center justify-center overflow-hidden rounded-lg bg-gray-300 md:mb-4 mr-0">
+                      <img
+                        src={
+                          practitioner.image_url?.replaceAll('"', "") ||
+                          "/placeholder.svg"
+                        }
+                        alt="Product photo"
+                        className="object-cover rounded-lg min-w-full min-h-full"
+                      />
+                    </div>
+
+                    <div className="flex items-start md:items-center flex-col pl-4 md:pl-0 w-[calc(100%-80px)] md:w-full">
+                      {practitioner.product_name && (
+                        <p className="flex items-center gap-1 rounded-full bg-green-100 text-green-800 border border-gray-200 text-[10px] px-3 py-1 mb-2">
+                          {decodeUnicodeEscapes(practitioner?.distributor_cleaned!.trim())}
+                        </p>
+                      )}
+
+                      <h3 className="mb-2 md:mb-0 flex text-left md:text-center md:align-items-center md:justify-center font-semibold text-xs md:text-md leading-relaxed text-balance line-clamp-2">
+                        {decodeUnicodeEscapes(practitioner.product_name)}
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </CardHeader>
+
+            <CardContent className="pt-0 px-0 md:px-4 space-y-4">
+              <div className="flex md:items-center md:justify-center gap-2 text-[11px] text-gray-600">
+                <span className="text-pretty text-center">
+                  {decodeUnicodeEscapes(practitioner.category!.trim())}
+                </span>
+              </div>
+              <div>
+                <ul className="flex flex-wrap md:items-center md:justify-center gap-1 text-center" aria-label="Product prices">
+                  {true &&
+                    practitioner.all_prices.map((value: any, index: number) => (
+                      <li key={index}>
+                        <Badge variant="outline" className="text-[11px] font-normal text-gray-500">
+                          {value.price}
+                        </Badge>
+                      </li>
+                    ))}
+                  {/* {true &&
+              practitioner.reviewAnalysis?.procedures_offered?.categories
+                ?.length! > 2 && (
+                <Badge variant="outline" className="text-xs">
+                  +
+                  {practitioner.reviewAnalysis?.procedures_offered?.categories!
+                    .length! - 2}{" "}
+                  more
+                </Badge>
+              )} */}
+                </ul>
+              </div>
+            </CardContent>
+          </Card>
+        </Link>
+
+            </div>
+          ))}
+        </div>
       
       </div>
       
